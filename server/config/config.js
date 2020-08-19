10c721416d971b17e8ca4f379e27d1b813c34fdc
Daniel <dcastro.espido@gmail.com>
2020-08-19 19:27:03 +0200

    Google Sing-In

--- server/config/config.js
+++ server/config/config.js
@@ -34,3 +34,8 @@ if (process.env.NODE_ENV === 'DEV') {
     urlDB = process.env.MONGO_DB_URI;
 }
 process.env.URLDB = urlDB;
+
+// ====================
+// Google Client ID
+// ====================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '1073694397533-5bqeqtd86tldg2nd9pmm0nfko966of71.apps.googleusercontent.com'
